0fa732085a3e52a9cc9641191cbc8fd7272e3223
Robert Smith <oakcodes@gmail.com>
2018-06-02 10:51:23 -0400

    Generic Collection Dictionary Example

---
+++ C#/DictionnaryDemo/DictionnaryDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DictionnaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/DictionnaryDemo/DictionnaryDemo/Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionnaryDemo
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+
+        public void Work()
+        {
+            Dictionary<string, Person> dict = new Dictionary<string, Person>();
+
+            Person george = new Person() { Name = "George Washington", Age = 67 };
+            string key = "George";
+            dict.Add(key, george);
+
+            dict.Add("john", new Person() { Name = "John ADAMS", Age = 90 });
+            dict.Add("Thom", new Person() { Name = "Thomas Jefferson", Age = 91 });
+            dict.Add("james", new Person() { Name = "John Adams", Age = 90 });
+
+            Person thirdPresident = dict["Thom"];
+            Console.Write("The third president of the United State was :" + thirdPresident.Name);
+
+        }
+    }
+}
